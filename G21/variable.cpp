fa6d4f2ab080bc61aefff58a63c8e9f5f24a5dd3
sakshibadyal <sakshibadyal09@gmail.com>
2025-07-09 11:53:35 +0530

    cpp

--- G21/variable.cpp
+++ G21/variable.cpp
@@ -1172,20 +1172,23 @@
 // }
 
 
-#include <iostream>
-using namespace std;
-int main() {
-    int n;
-    cout << "Enter a positive integer: ";
-    cin >> n;
-  int sum=0;
-  int rev=0;
-while(n>0){
-    int ld=n%10;
-    rev=rev*10+ld;
-  
-    n=n/10;
-}
-  cout<<rev;
-    return 0;
-}
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int n;
+//     cout << "Enter a positive integer: ";
+//     cin >> n;
+//   int sum=0;
+//   int rev=0;
+// while(n>0){
+//     int ld=n%10;
+//     rev=rev*10+ld;
+//     n=n/10;
+// }
+//   cout<<rev;
+// return 0;
+// }
+
+
+
+//git checkup
