16beb0cc788a2212cc28a9cc586425ffc2524436
sakshibadyal <sakshibadyal09@gmail.com>
2025-07-23 19:07:49 +0530

    ADD

--- G21/lect.cpp
+++ G21/lect.cpp
@@ -80,3 +80,122 @@
 // print("India");
 // }
 
+
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     int a=0,b=1;
+//     for(int i=1;i<=n;i++){
+//         cout<<a<<(i<n?" ":"");
+//         int c=a+b;
+//         a=b;
+//         b=c;
+//     }
+//     return 0;
+// }
+
+
+
+
+// 
+
+
+
+
+
+
+
+// #include <iostream>
+// using namespace std;
+// void fibo(int n){
+//     int a=0,b=1;
+//     for(int i=0; i<n;i++){
+//         cout<<a<<" ";
+//         int c=a+b;
+//         a=b;
+//         b=c;
+//     }
+// }
+
+// void prime(int n){
+//     if(n<=1) {
+//         cout<<"Not Prime";
+//         return;
+//     }
+//     for(int i=2;i<=n/2;i++){
+//         if(n % i==0){
+//             cout<<"Not Prime";
+//             return;
+//         }
+//     }
+//     cout<<"Prime";
+// }
+// int main(){
+//     int n;
+//     cin>>n;
+//      cout<<"Fibonacci Series till "<< n << " is"<<" ";
+//     fibo(n);
+//     cout<<endl;
+//     cout<<n << " is "<<" ";
+//     prime(n);
+//     return 0;
+// }
+
+
+
+// #include <iostream>
+// using namespace std;
+// int countDigits(int n) {
+//     int count = 0;
+//     if (n == 0) return 1;
+//     if (n < 0) n = -n;
+//     while (n > 0) {
+//         n /= 10;
+//         ++count;
+//     }
+//     return count;
+// }
+
+// int main() {
+//     int n;
+//     cin >> n;
+//     cout << countDigits(n);
+//     return 0;
+// }
+
+
+
+// #include <iostream>
+// using namespace std;
+// int factorial(int n) {
+//     int res = 1;
+//     for (int i = 2; i <= n; ++i)
+//         res *= i;
+//     return res;
+// }
+// int main() {
+//     int n;
+//     cin >> n;
+//     cout << factorial(n);
+//     return 0;
+// }
+
+
+
+// #include <iostream>
+// #include <cmath>
+// using namespace std;
+// double compoundInterest(double p, double r, double t) {
+//     return p * pow(1 + r / 100, t) - p;
+// }
+// int main() {
+//     double p, r, t;
+//     cin >> p >> r >> t;
+//     cout << compoundInterest(p, r, t);
+
+// }
+
+
