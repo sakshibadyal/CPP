8448f2a2e50a2b3709391c08229e55e5c44f7785
sakshibadyal <sakshibadyal09@gmail.com>
2025-07-24 11:41:52 +0530

    Fun

---
+++ G21/fun.cpp
@@ -0,0 +1,62 @@
+// #include<iostream>
+// using namespace std;
+// int add(int a,int b){
+//     int sum=a+b;
+//     return sum;
+// }
+// float sum(int a,int b){
+//     int add=a+b;
+//     return add;
+// }
+// int main(){
+    // int a,b;
+    // cin>>a>>b;
+    // cout<<add(a,b)<<endl;
+    // float a,b;
+    // cin>>a>>b;
+    // cout<<sum(a,b)<<endl;
+// }
+
+
+
+
+// #include<iostream>
+// using namespace std;
+// int sq(int n){
+//     return n*n;
+
+// }
+// int main(){
+//     int n;
+//     cout<<"Enter a number:";
+//     cin>>n;
+//     for(int i=1;i<=n;i++){
+//         cout<<sq(i)<<"\n";
+//     }
+// }
+
+
+
+
+// #include<iostream>
+// using namespace std;
+// double circumference(int r){
+//     return 2*3.14*r;
+
+// }
+// double area(int r){
+//     return 3.14*r*r;
+// }
+// int sq(int r){
+//     return r*r;
+// }
+
+// int main(){
+//     int r;
+//     cout<<"Enter a number:";
+//     cin>>r;
+//     cout<<circumference(r)<<endl;
+//     cout<<area(r)<<endl;
+//     cout<<sq(r);
+
+// }
